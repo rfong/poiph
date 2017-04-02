7d19f8c71b3c7a4ec7f2147be1991172e5c8de1d
willow <tolstoyknows@gmail.com>
2017-04-02 00:58:32 -0700

    made work with stick 1 of xbox controller

--- README.md
+++ README.md
@@ -9,6 +9,17 @@ not to be confused with my [noninteractive polar poi pattern visualizer](http://
 + ~make poi handles movable~
 + ~sanity test antispin~
 + ~make poi handles movable with mouse input~
-+ implement polar movement (controls for each hand = timing and radius)
-+ pipe xbox controller to controller
++ ~pipe xbox controller to controller~
++ add second poi connected to second stick
++ add poi head traces
+
+
+Later-
++ option for poi to spin constant speed
+	+stick button changes direction of spin
++
++ add fun button functionalities
+	+stall
+	+colors/visual designs
++ change tethers to flacid floppy flops.
 + import matter.js cleanly
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 /* Controllers for poi game. */
 
+var CIRCLE_X = 500;
+var CIRCLE_Y = 500;
+var CIRCLE_R = 300;
+var GAMEPAD_INTERVAL = 25;  // millis
+
 function gameLoop() {
     /* todo: look at
      * Matter.Engine.update
@@ -21,9 +26,6 @@ function onMouseUpdate(e) {
 
 function circleLimiter(x, y) {
     /* limits the motion of the poi to within a circle */
-    var CIRCLE_X = 500;
-    var CIRCLE_Y = 500;
-    var CIRCLE_R = 300;
 
     var r = Math.sqrt(Math.pow(x - CIRCLE_X, 2) + Math.pow(y - CIRCLE_Y, 2));
     if (r <= CIRCLE_R) {
@@ -38,5 +40,86 @@ function circleLimiter(x, y) {
 }
 
 
-document.addEventListener('mousemove', onMouseUpdate, false);
-document.addEventListener('mouseenter', onMouseUpdate, false);
+//document.addEventListener('mousemove', onMouseUpdate, false);
+//document.addEventListener('mouseenter', onMouseUpdate, false);
+
+
+
+/* our code for dealing with gamepads */
+
+function onGamepadChange() {
+    ///reportOnGamepad(); //game pad tester bit
+
+    var gp = navigator.getGamepads()[0]; // TODO safety checks (is it connected, does one exist)
+    
+    var stick1 = {
+        x: gp.axes[0] * CIRCLE_R + CIRCLE_X,
+        y: gp.axes[1] * CIRCLE_R + CIRCLE_Y,
+    };
+
+    console.log(stick1);
+    var newCoords = circleLimiter(stick1.x, stick1.y);
+    dirtyConstraint.pointA.x = newCoords[0];
+    dirtyConstraint.pointA.y = newCoords[1];
+}
+
+
+
+/* example code for dealing with gamepads */
+
+var hasGP = false;
+var repGP;
+
+function canGame() {
+    return "getGamepads" in navigator;
+}
+
+function reportOnGamepad() {
+    var gp = navigator.getGamepads()[0];
+    var html = "";
+        html += "id: "+gp.id+"<br/>";
+
+    for(var i=0;i<gp.buttons.length;i++) {
+        html+= "Button "+(i+1)+": ";
+        if(gp.buttons[i].pressed) html+= " pressed";
+        html+= "<br/>";
+    }
+
+    for(var i=0;i<gp.axes.length; i+=2) {
+        html+= "Stick "+(Math.ceil(i/2)+1)+": "+gp.axes[i]+","+gp.axes[i+1]+"<br/>";
+    }
+
+    $("#gamepadDisplay").html(html);
+}
+
+$(document).ready(function() {
+
+    if(canGame()) {
+
+        var prompt = "To begin using your gamepad, connect it and press any button!";
+        $("#gamepadPrompt").text(prompt);
+
+        $(window).on("gamepadconnected", function() {
+            hasGP = true;
+            $("#gamepadPrompt").html("Gamepad connected!");
+            console.log("connection event");
+            repGP = window.setInterval(onGamepadChange, GAMEPAD_INTERVAL);
+        });
+
+        $(window).on("gamepaddisconnected", function() {
+            console.log("disconnection event");
+            $("#gamepadPrompt").text(prompt);
+            window.clearInterval(repGP);
+        });
+
+        //setup an interval for Chrome
+        var checkGP = window.setInterval(function() {
+            console.log('checkGP');
+            if(navigator.getGamepads()[0]) {
+                if(!hasGP) $(window).trigger("gamepadconnected");
+                window.clearInterval(checkGP);
+            }
+        }, 500);
+    }
+
+});
